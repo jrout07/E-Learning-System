783d28e3a801a9f8ac13519ec06afdf3399ba774
Jyoti Ranjan Rout <routj8260@gmail.com>
2025-04-21 14:14:49 +0530

    Update DBconnection.java

---
+++ src/db/DBconnection.java
@@ -0,0 +1,16 @@
+package db;
+import java.sql.*;
+
+public class DBconnection {
+    private static final String URL ="jdbc:mysql://localhost:3306/e_learning";
+    private static final String USER ="root";
+    private static final String PASSWORD ="2004";
+
+    public static Connection getConnection(){
+        try {
+            return DriverManager.getConnection(URL,USER,PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
